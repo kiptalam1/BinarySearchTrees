7581b37d53acaa27c396630b00243d1ad4c58d13
adams <adamskiptalam0@gmail.com>
2025-01-21 14:23:35 +0300

    Functionality to sort array and remove duplicates

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,4 +10,13 @@ class Tree {
         this.array = array;
         this.root = null;
     }
+    buildTree(array) {
+        const sortedArray = [...new Set(array)].sort((a, b) => a-b);
+        return sortedArray;
+    }
+    
 }
+
+
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+// console.log(buildTree(arr))
