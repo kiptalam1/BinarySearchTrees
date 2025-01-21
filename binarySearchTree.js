2f2ae0a5359c85f15d73bec3c3d87ffed7596935
adams <adamskiptalam0@gmail.com>
2025-01-21 14:00:41 +0300

    Build a Node and Tree class

---
+++ binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor (data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+class Tree {
+    constructor (array) {
+        this.array = array;
+        this.root = null;
+    }
+}
